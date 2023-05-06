acffe8ae024cbb63a984a5515b13970bbc5b4b57
Praneeth <praneethvaleti1@gmail.com>
2023-05-06 18:06:10 +0530

    Removed Unwanted Code

--- src/main/java/com/sample/demo/DemoApplication.java
+++ src/main/java/com/sample/demo/DemoApplication.java
@@ -3,23 +3,15 @@ package com.sample.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.multipart.support.MultipartFilter;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @Configuration
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	public MultipartResolver multipartResolver() {
-		return new CommonsMultipartResolver();
-	}
 
 }
